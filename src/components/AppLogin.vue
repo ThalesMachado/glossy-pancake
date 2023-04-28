84d9cd46ed11ae731b59bba8df51eb1629125063
Thales Machado <thalesm@id.uff.br>
2023-04-27 23:05:48 -0300

    tela de login

--- src/components/AppLogin.vue
+++ src/components/AppLogin.vue
@@ -1,5 +1,25 @@
 <template>
     <div>
-        <h1>Olá, teste!</h1>
+        <input type="email" name="email" id="email" placeholder="joao@teste.com" v-model="email">
     </div>
+    <div>
+        <input type="password" name="password" id="password" v-model="password">
+    </div>
+    <button @click="login">Entrar</button>
 </template>
+
+<script>
+export default{
+    data() {
+        return {
+            email: '',
+            password: ''
+        }
+    },
+    methods: {
+        login(event) {
+            console.log(this.email)
+        }
+    }
+}
+</script>
